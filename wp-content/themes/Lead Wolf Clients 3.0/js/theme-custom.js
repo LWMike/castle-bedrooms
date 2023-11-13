c05b6d9bff00fd99f04afe3e04165c5794747804
LWMike <150452934+LWMike@users.noreply.github.com>
2023-11-13 17:49:50 +0000

    Updated/Added simply-static-125-1699897560

--- wp-content/themes/Lead Wolf Clients 3.0/js/theme-custom.js
+++ wp-content/themes/Lead Wolf Clients 3.0/js/theme-custom.js
@@ -1,7 +1,1 @@
-//Change menu tabs behaviour from click to hover
-const megaMenuTabs = document.querySelectorAll('.styles-mega-menu .kt-tabs-title-list li a');
-megaMenuTabs.forEach(function(e){
-    e.addEventListener('mouseover', function() {
-      e.click();
-    });
-});
+const megaMenuTabs=document.querySelectorAll('.styles-mega-menu .kt-tabs-title-list li a');megaMenuTabs.forEach(function(e){e.addEventListener('mouseover',function(){e.click();});});
